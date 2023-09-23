e2ccd2e19b63a26a669f26ea257d444c3b09d147
Run Dreamer <18752111+RunDreamer@users.noreply.github.com>
2023-09-23 15:17:47 +0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,111 +1,93 @@
 let mic;
-let input;
 let fft;
+let mySound1, mySound2, mySound3, mySound4;
+let xgnr;
 
-function preload() {
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
+  
+  let micButton = createButton('Start microphone');
+  micButton.position(10, 10);  // Position the button at (10, 10)
+  micButton.mousePressed(startMic);
+}
 
-  // Create an Audio input
-
-
-  var context = new AudioContext();
-  // context.createGain();
-
-  input = new p5.AudioIn();
-  input.start();
-  input.amp(0.5);
+async function startMic() {
+  // Initialize the audio in the startMic function
   soundFormats('mp3', 'ogg');
   mySound1 = loadSound('sh11.mp3');
   mySound2 = loadSound('sh12.mp3');
   mySound3 = loadSound('sh13.mp3');
   mySound4 = loadSound('sh14.mp3');
 
-  // fullscreen();
+  // Create the AudioContext after the user clicks the button
+  await userStartAudio();
 
-}
-
-function setup() {
-  
-  createCanvas(windowWidth, windowHeight);
-  noStroke();
-  
   mic = new p5.AudioIn();
   mic.start();
-
   fft = new p5.FFT();
-  amplitude = new p5.Amplitude();
   fft.setInput(mic);
   mySound1.setVolume(0.3);
   mySound2.setVolume(0.3);
   mySound3.setVolume(0.3);
   mySound4.setVolume(0.3);
-
   xgnr = 0;
 
+  // Resume the AudioContext
+  if (getAudioContext().state !== 'running') {
+    getAudioContext().resume();
+  }
 
-
+  this.elt.style.transition = "opacity 1s";  // Add a CSS transition to the button
+  this.elt.style.opacity = 0;  // Set the button's opacity to 0 to fade it out
+  setTimeout(() => this.remove(), 1000);  // Remove the button after 1 second
 }
 
 function draw() {
-  
   resizeCanvas(windowWidth, windowHeight);
   background(0);
 
-  // micLevel = mic.getLevel();
+  if (fft) {
+    fft.analyze();
+    let gnr = fft.getEnergy(2500, 8500);
+    xgnr = (gnr * 1.47 + xgnr) * 0.974;
 
-  // console.log("mic level: " + micLevel)
-  // analyze = fft.analyze();
+    let tresh1 = 1900;
+    let tresh2 = 3300;
+    let tresh3 = 3700;
+    let tresh4 = 4000;
 
-  fft.analyze()
-  let gnr = fft.getEnergy(2500, 8500);
-  // console.log("energy: " + gnr);
-  xgnr = (gnr * 1.47 + xgnr) * 0.974;
+    if (xgnr > tresh1) {
+      mySound1.playMode('untilDone');
+      mySound1.play();
+    }
 
-  let tresh1 = 1900;
-  let tresh2 = 3300;
-  let tresh3 = 3700;
-  let tresh4 = 4000;
+    if (xgnr > tresh2) {
+      mySound2.playMode('untilDone');
+      mySound2.play();
+    }
 
-  console.log(xgnr);
+    if (xgnr > tresh3) {
+      mySound3.playMode('untilDone');
+      mySound3.play();
+    }
 
-  if (xgnr > tresh1) {
-    mySound1.playMode('untilDone');
-    mySound1.play();
+    if (xgnr > tresh4) {
+      mySound4.playMode('untilDone');
+      mySound4.play();
+      xgnr = 0;
+    }
 
+    let c = map(xgnr, 0, 2000, -250, 100);
+    let d = map(xgnr, 0, 1000, 0, 250);
+
+    fill(250, 250, 250, c);
+    ellipse(width / 2, height / 2, d / 3, d / 3);
+
+    fill(255, 40, 0);
+    ellipse(width / 2, height / 2, d / 10, d / 10);
+
+    fill(255, 0, 0, c / 2);
+    ellipse(width / 2, height / 2, d * 1.1, d * 1.1);
   }
-
-  if (xgnr > tresh2) {
-    mySound2.playMode('untilDone');
-    mySound2.play();
-
-  } // xgnr = 0;
-
-  if (xgnr > tresh3) {
-    mySound3.playMode('untilDone');
-    mySound3.play();
-
-    // xgnr = 0;
-  }
-  if (xgnr > tresh4) {
-    mySound4.playMode('untilDone');
-    mySound4.play();
-
-    xgnr = 0;
-
-  }
-
-
-  let c = map(xgnr, 0, 2000, -250, 100);
-  let d = map(xgnr, 0, 1000, 0, 250);
-  let e = map(xgnr, 0, 1000, 11, 100);
-
-
-  fill(250, 250, 250, c);
-  ellipse(width / 2, height / 2, d / 3, d / 3);
-
-  fill(255, 40, 0);
-  ellipse(width / 2, height / 2, d / 10, d / 10);
-
-  fill(255, 0, 0, c / 2);
-  ellipse(width / 2, height / 2, d * 1.1, d * 1.1);
-
 }
